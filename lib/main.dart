3d1ea88ac8658e37e7d7bcd695ad4640a252f6c7
Prince452707 <princekumar22510@gmail.com>
2024-08-08 23:21:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1364,30 +1364,6 @@
 //   }
 // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import 'dart:math';
 import 'package:shimmer/shimmer.dart';
 import 'package:cached_network_image/cached_network_image.dart';
